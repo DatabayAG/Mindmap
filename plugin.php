60206dee88ce852957e79eae4d2a17d8c2e78bcd
ay <ay@databay.de>
2019-07-24 11:40:05 +0200

    update auf 5.4

--- plugin.php
+++ plugin.php
@@ -4,13 +4,13 @@
 $id = "xmmp";
  
 // code version; must be changed for all code changes
-define('xmmp_version', "1.4.0");
+define('xmmp_version', "1.4.1");
 $version = xmmp_version;
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "5.3.999";
+$ilias_max_version = "5.4.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Aresch Yavari";
