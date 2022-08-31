0f159fd6849eb06bcd55ef3a24b1f47d231f40de
Aresch Yavari <ay@databay.de>
2022-08-31 11:04:39 +0200

    update auf 7

--- plugin.php
+++ plugin.php
@@ -10,7 +10,7 @@ $version = xmmp_version;
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "6.9.999";
+$ilias_max_version = "7.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Aresch Yavari";
